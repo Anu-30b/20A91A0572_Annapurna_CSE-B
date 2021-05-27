e9f12898b1fca8bf52966068779d912cda996779
Anub-30 <84388912+Anub-30@users.noreply.github.com>
2021-05-27 12:42:31 +0530

    Add files via upload

---
+++ while loop.py
@@ -0,0 +1,18 @@
+i=int(input('enter first value'))
+n=int(input('enter end value'))
+b=int(input('enter how much to be added'))
+while (i<=n):
+    print(i)
+    i=i+b
+
+
+##Output :
+##    enter first value 1
+##    enter end value 10
+##    enter how much to be added 2
+##    1
+##    3
+##    5
+##    7
+##    9
+##    
